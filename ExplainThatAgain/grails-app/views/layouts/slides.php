a9a6c3dfc13ccfec0d041807f94a86cabb19b18a
psalv <psalvato@uwo.ca>
2017-02-21 18:20:17 -0500

    Removing php

--- ExplainThatAgain/grails-app/views/layouts/slides.php
+++
@@ -1,7 +0,0 @@
-
-<!--
-
-This file will contain all content related to the slides.
-When ran, this should JUST show the slides on the screen, so that it can be appropriately placed into mainArea.php
-
--->
