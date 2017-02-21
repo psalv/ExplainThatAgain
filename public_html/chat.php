960019c79587299e19dd5a6c1f8f73d35008e748
psalv <psalvato@uwo.ca>
2017-02-21 12:01:01 -0500

    Structured files for session, added necessary assets.

---
+++ public_html/chat.php
@@ -0,0 +1,10 @@
+
+<!--
+
+This file will contain all content related to the chat.
+When ran, this should JUST show a chat on the screen, so that it can be appropriately placed into mainArea.php.
+
+The chat should be self contained, so one should be able to enter messages and have them appear on their screen by
+running this file alone.
+
+-->
