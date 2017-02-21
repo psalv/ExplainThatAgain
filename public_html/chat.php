4508a84d0db3b672732ce4a9143413af7fb57605
psalv <psalvato@uwo.ca>
2017-02-21 12:03:21 -0500

    Structured files for session, added necessary assets for the frontend.

--- public_html/chat.php
+++ public_html/chat.php
@@ -7,4 +7,5 @@ When ran, this should JUST show a chat on the screen, so that it can be appropri
 The chat should be self contained, so one should be able to enter messages and have them appear on their screen by
 running this file alone.
 
+
 -->
