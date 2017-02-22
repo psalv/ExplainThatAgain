4996f3091de9d0dab4fd22c1b24a5fee3021691c
psalv <psalvato@uwo.ca>
2017-02-21 21:10:47 -0500

    Fixed grails application so that it works by recreating the applet.

--- ExplainThatAgain/grails-app/views/layouts/graph.php
+++
@@ -1,7 +0,0 @@
-
-<!--
-
-This file will contain all content related to the actual graph itself.
-When ran, this should JUST show a graph on the screen, so that it can be appropriately placed into mainArea.php
-
--->
