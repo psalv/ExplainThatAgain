3620c41d630d7cfc8ff83bab3f62c67fc4dc8847
schindlerjake <jschind@uwo.ca>
2017-03-01 10:55:10 -0500

    added prof back

---
+++ grails-app/domain/cs2212group14/Professor.groovy
@@ -0,0 +1,11 @@
+package cs2212group14
+
+class Professor extends User {
+
+    //static hasMany = [classesTeaching: Class]
+
+    static constraints = {
+        //classesTeaching nullable:true
+    }
+
+}
