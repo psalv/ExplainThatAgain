813db60b04c9476a3afac4088ad015677f021691
Martin <mvieir4@uwo.ca>
2017-03-03 12:07:27 -0500

    Stuff for profile

---
+++ src/main/js/addClass.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import 'whatwg-fetch';
+import {withRouter} from 'react-router';
+import ReactDOM from 'react-dom';
+
+const checkStatus = (response) => {
+    if(response.status >= 200 && response.status < 300) {
+        return response.json();
+    } else {
+        var error = new Error(response.statusText);
+        error.response = response;
+        throw error;
+    }
+};
+
+class AddClass extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            name: '',
+            error: ''
+        };
+        this.addClass = this.addClass.bind(this);
+    }
+
+    addClass(e) {
+        e.preventDefault();
+        let form = this.form.data();
+        console.log("Adding...", form);
+
+        let body = "Name for class=" + form.username
+
+        fetch("/api/admin/createSession", {
+            method: 'POST',
+            headers: {
+                "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+            },
+            body: body
+        })
+            .then(checkStatus)
+            .then(this.success.bind(this))
+            .catch(this.readError.bind(this))
+            .then(this.fail.bind(this))
+    }
+
+    success(user) {
+        console.log("Added class", user);
+        this.props.router.replace("/");
+    }
+
+    readError(error) {
+        console.log("Failed to add class", error);
+        return error.response.json();
+    }
+
+    fail(error) {
+        if(error) this.setState({error: error.error});
+    }
+
+    render () {
+        let Error = () => <p className="alert alert-danger">{this.state.error}</p>;
+        return (
+
+            <div className="col-sm-4 col-sm-offset-4">
+                { this.state.error ? <Error/> : null }
+                <UserForm submitLabel="Add Class" onSubmit={this.addClass} ref={ (ref) => this.form = ref }/>
+            </div>
+        )
+    }
+
+}
+
+export default withRouter(AddClass);
+
