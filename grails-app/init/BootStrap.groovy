258a18142096cf5e2c457b043f83bed73666777f
r-meagher <rmeaghe2@uwo.ca>
2017-03-03 21:07:32 -0500

    Revert "Started merging with main branch"
    This reverts commit f4dcca4a090f59e742591781529dae52e900090e.

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,6 +1,3 @@
-import com.explainthatagain.Course
-import com.explainthatagain.Graph
-import com.explainthatagain.Session
 import com.explainthatagain.Role
 import com.explainthatagain.User
 import com.explainthatagain.UserRole
@@ -11,17 +8,6 @@ class BootStrap {
         def role = new Role(authority: 'ROLE_USER').save()
         def user = new User(username: 'test', password: '2212').save()
         UserRole.create(user, role, true)
-
-        new User(realname: "test", username: "test", password: "test", email: "test@test.ca", verified: true).save(flush: true, failOnError: true)
-        new User(realname: "test", username: "test123", password: "test123", email: "test@test.ca", verified: true).save(flush: true, failOnError: true)
-        new Course(user: User.findByRealname("test"), courseName: "test").save(flush: true, failOnError: true)
-        new Session(course: Course.findByCourseName("test"), sessionID: 1, sessionName: 'test', active: true).save(flush: true, failOnError: true)
-        new Graph(session: Session.findBySessionID(1), graphID: 1).save(flush: true, failOnError: true)
-
-        println User.findAll()
-        println Course.findAll()
-        println Session.findAll()
-        println Graph.findAll()
     }
     def destroy = {
     }
