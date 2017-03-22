41a07c8f5af15f66849dc16a6918bb3ec1968c7c
psalv <psalvato@uwo.ca>
2017-03-22 02:53:05 -0400

    Don't understand button is functional in terms of changes to mysql db

--- public_html/controller/incrementGraph.php
+++ public_html/controller/incrementGraph.php
@@ -1,7 +1,54 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: paulsalvatore57
- * Date: 2017-03-22
- * Time: 1:34 AM
- */
+
+
+if($_SERVER['REQUEST_METHOD'] === 'GET'){
+    header("HTTP/1.1 403 Forbidden");
+    exit;
+}
+
+include_once("../../env.php");
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+$getPost = (array)json_decode(file_get_contents('php://input'));
+$sessionID = $getPost['sessionID'];
+$table = "Graph_" . $sessionID;
+
+
+$sql = "SELECT MAX(id) as id FROM " . $table;
+$result = $conn->query($sql);
+$result = $result->fetch_assoc();
+
+
+if($result !== NULL){
+    $id = $result['id'];
+
+    $sql = "SELECT confusion FROM " . $table . " WHERE id=" . $id;
+    $result = $conn->query($sql);
+    $result = $result->fetch_assoc();
+
+    $sql = "UPDATE " . $table . " SET confusion=" . (intval($result['confusion']) + 1) . " WHERE id=" . $id;
+    if($conn->query($sql) === TRUE){
+        echo json_encode(array('success' => true, 'message' => 'You are confused'));
+    }
+    else{
+        echo json_encode(array('success' => false, 'message' => 'Could not increment confusion'));
+    }
+
+}
+else{
+    echo json_encode(array('success' => false, 'message' => 'No slides exist'));
+}
+
+
+$conn->close();
+
+
+
